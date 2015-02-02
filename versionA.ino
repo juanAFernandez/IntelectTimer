9b16ec07a50fb5b23cba8eeeeb60a34514e86e8c
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2015-02-02 23:16:45 +0100

    Add code versionA.ino

---
+++ versionA.ino
@@ -0,0 +1,68 @@
+/*
+ Based on open-source code  Blink
+*/
+
+void setup() {                
+  // initialize the digital pin as an output.
+  // Pin 13, 12, 8 and 7  has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);     
+  
+  pinMode(12, OUTPUT);
+  
+  pinMode(8, OUTPUT);
+  
+  pinMode(7, OUTPUT);
+
+  pinMode(3, INPUT);
+}
+
+boolean apagar=false;
+int botonState=0;
+
+void loop() {
+
+  //digitalWrite(7,HIGH);
+  
+   
+  digitalWrite(7, HIGH);
+  delay(10000);
+  digitalWrite(8, HIGH);
+  delay(10000);
+  digitalWrite(12, HIGH);
+  delay(10000);
+  digitalWrite(13, HIGH);
+  delay(10000);
+  
+  do{
+    
+    delay(100);
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(13,LOW);
+    delay(100);
+    digitalWrite(7,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(13,HIGH);
+     
+    botonState=digitalRead(3); 
+     
+  }while(botonState==LOW);
+  
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
+  delay(500);
+  
+  
+  
+  /*
+  digitalWrite(13, HIGH);   // set the LED on
+  digitalWrite(12, HIGH);   // set the LED green two on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);              // wait for a second
+  */
+}
